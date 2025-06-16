3fa9e7ea7565325c15c27a186368739c2b0dbebc
Trey Cole <tbjcole@outlook.com>
2025-06-16 23:14:20 +0200

    update namespace

--- pythtb/__init__.py
+++ pythtb/__init__.py
@@ -16,12 +16,26 @@ __author__ = "Trey Cole, Sinisa Coh, David Vanderbilt"
 __license__ = "GPL-3.0"
 
 # Import all public API from the core module
-from .pythtb import *
+from .tb_model import *
+from .wf_array import *
+from .w90 import *
+from .k_mesh import *
+from .wannier import *
+from .utils import *
 
-# Use the core module's __all__ to define the package exports
-# This ensures 'from pythtb import *' pulls in the same symbols
-from . import pythtb as _core
-__all__ = getattr(_core, "__all__", [name for name in globals() if not name.startswith("_ ")])
+from . import tb_model, wf_array, w90, k_mesh, wannier, utils
 
-# if pythtb.py defines __all__, that will control what “import *” pulls in.
-# otherwise, Python will export all names that don’t start with an underscore.
+__all__ = []
+__all__ += tb_model.__all__
+__all__ += wf_array.__all__
+__all__ += w90.__all__
+__all__ += k_mesh.__all__
+__all__ += wannier.__all__
+__all__ += utils.__all__
+
+# Use the core module's __all__ to define the package exports from * imports.
+# This ensures 'from pythtb import *' pulls in only the intended public API.
+# If you want to control what gets imported with "from pythtb import *",
+# you can define __all__ in the respective modules (tb_model, wf_array, w90).
+# This is a common practice in Python packages to avoid polluting the namespace
+# with internal details and to provide a clear public API.
