43f242d05d0d856b9cf8cb8b61a518ae01219df7
Trey Cole <tbjcole@outlook.com>
2025-06-17 15:12:29 +0200

    generalize reshape in position matrix

--- pythtb/tb_model.py
+++ pythtb/tb_model.py
@@ -2156,7 +2156,10 @@ somehow changed Cartesian coordinates of orbitals."""
             # tile along spin direction if needed
             pos_use = np.tile(pos_tmp, (2, 1)).transpose().flatten()
             # also flatten the state along the spin index
-            evec_use = evec.reshape((evec.shape[0], evec.shape[1] * evec.shape[2]))
+            #TODO: check if this is correct
+            # evec is k, band, orbital, spin
+            # evec_use = evec.reshape((evec.shape[0], evec.shape[1] * evec.shape[2]))
+            evec_use = evec.reshape((*evec.shape[:-2], -1))
         else:
             pos_use = pos_tmp
             evec_use = evec
